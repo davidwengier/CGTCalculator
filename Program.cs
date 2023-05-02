61a08998af0da46f5efbfb9d0b4cbb2f7cb445ad
David Wengier <david.wengier@microsoft.com>
2023-05-02 21:43:54 +1000

    Clean up

--- Program.cs
+++ Program.cs
@@ -1,17 +1,47 @@
-namespace CGTCalculator
+using Microsoft.AspNetCore.Components.WebView.WindowsForms;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CGTCalculator;
+
+internal static class Program
 {
-    internal static class Program
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new MainForm());
-        }
+        ApplicationConfiguration.Initialize();
+        
+        var services = ConfigureServices();
+
+        var form = CreateForm(services);
+
+        Application.Run(form);
+    }
+
+    private static ServiceProvider ConfigureServices()
+    {
+        var services = new ServiceCollection();
+        services.AddWindowsFormsBlazorWebView();
+        return services.BuildServiceProvider();
+    }
+
+    private static Form CreateForm(ServiceProvider serviceProvider)
+    {
+        var form = new Form();
+
+        var webView = new BlazorWebView();
+        webView.Dock = DockStyle.Fill;
+        form.Controls.Add(webView);
+
+        webView.HostPage = "wwwroot\\index.html";
+        webView.Services = serviceProvider;
+        webView.RootComponents.Add<Counter>("#app");
+
+        form.Size = new Size(800, 600);
+        form.Text = "CGT Calculator";
+
+        return form;
     }
 }
