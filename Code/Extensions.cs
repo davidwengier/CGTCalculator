d0524dad03d305e17782908d744ba32f62cd7fd4
David Wengier <david.wengier@microsoft.com>
2023-05-09 17:53:33 +1000

    Add form to Add screen, and nav properly

---
+++ Code/Extensions.cs
@@ -0,0 +1,9 @@
+﻿namespace CGTCalculator;
+
+internal static class Extensions
+{
+    public static DateOnly ToDateOnly(this DateTime dateTime)
+    {
+        return new DateOnly(dateTime.Year, dateTime.Month, dateTime.Day);
+    }
+}
