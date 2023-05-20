20c9c8b9b30766ec1ea54836bebd653e8223c3f6
David Wengier <david.wengier@microsoft.com>
2023-05-20 21:33:24 +1000

    Allow editing and deleting

--- Pages/Add.razor.cs
+++ Pages/Add.razor.cs
@@ -1,25 +1,42 @@
-﻿namespace CGTCalculator.Pages;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+
+namespace CGTCalculator.Pages;
 
 public partial class Add
 {
-    private readonly TransactionModel _model = new();
+    [Parameter]
+    public Guid Id { get; set; } = Guid.Empty;
+
+    private Transaction _model = new()
+    {
+        Date = DateTime.Now.ToDateOnly()
+    };
+
+    private bool IsAdd => this.Id == Guid.Empty;
+
+    protected override async Task OnInitializedAsync()
+    {
+        if (!this.IsAdd)
+        {
+            _model = await this.DataSource.Transactions.Where(t => t.Id == this.Id).FirstAsync().ConfigureAwait(false);
+        }
+    }
 
     private async Task Add_Click()
     {
         var multiplier = _model.Type == TransactionType.Buy
             ? 1
             : -1;
-        var transaction = new Transaction
-        {
-            Id = Guid.NewGuid(),
-            Symbol = _model.Symbol,
-            Date = _model.Date,
-            Type = _model.Type,
-            Quantity = Math.Abs(_model.Quantity) * multiplier,
-            Value = Math.Abs(_model.Value) * multiplier,
-        };
+        _model.Quantity = Math.Abs(_model.Quantity) * multiplier;
+        _model.Value = Math.Abs(_model.Value) * multiplier;
 
-        await this.DataSource.Transactions.AddAsync(transaction).ConfigureAwait(false);
+        if (this.IsAdd)
+        {
+            _model.Id = Guid.NewGuid();
+            await this.DataSource.Transactions.AddAsync(_model).ConfigureAwait(false);
+        }
+
         await this.DataSource.SaveChangesAsync().ConfigureAwait(false);
         this.NavigationManager.NavigateTo("/");
     }
@@ -27,14 +44,5 @@ public partial class Add
     private void Cancel_Click()
     {
         this.NavigationManager.NavigateTo("/transactions");
-    }
-
-    private class TransactionModel
-    {
-        public string Symbol = "";
-        public DateOnly Date = DateTime.Now.ToDateOnly();
-        public TransactionType Type;
-        public decimal Quantity;
-        public decimal Value;
     }
 }
