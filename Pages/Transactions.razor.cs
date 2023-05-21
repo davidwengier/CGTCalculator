767cc028c5f5b16f825872bed240bbedd43d1580
David Wengier <david.wengier@microsoft.com>
2023-05-21 15:22:29 +1000

    Add Export button

---
+++ Pages/Transactions.razor.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Components.QuickGrid;
+using Microsoft.EntityFrameworkCore;
+
+namespace CGTCalculator.Pages;
+
+public partial class Transactions
+{
+    private QuickGrid<Transaction> _grid = null !;
+    private IQueryable<Transaction>? _transactions;
+
+    protected override Task OnInitializedAsync()
+    {
+        _transactions = this.Data.Transactions;
+        return Task.CompletedTask;
+    }
+
+    private void Add_Click()
+    {
+        this.NavigationManager.NavigateTo("/add");
+    }
+
+    private void Import_Click()
+    {
+        this.NavigationManager.NavigateTo("/import");
+    }
+
+    private async Task Export_Click()
+    {
+        using (var dlg = new SaveFileDialog())
+        {
+            dlg.Filter = "CSV Files|*.csv|All Files|*.*";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                var transactions = await _transactions!.ToListAsync();
+                await Exporter.ExportAsync(dlg.FileName, transactions);
+            }
+        }
+    }
+
+    private void Edit_Click(Transaction transaction)
+    {
+        this.NavigationManager.NavigateTo($"/edit/{transaction.Id}");
+    }
+
+    private async Task Delete_Click(Transaction transaction)
+    {
+        // Note to readers: You can't do this in a normal Blazor app :D
+        if (MessageBox.Show($"Are you sure to wish to delete this {transaction.Type} of {transaction.Symbol} for {transaction.Value:$#,##0.00}?", "Are you sure?", MessageBoxButtons.YesNo) == DialogResult.Yes)
+        {
+            this.Data.Remove(transaction);
+            await this.Data.SaveChangesAsync();
+            await _grid.RefreshDataAsync().ConfigureAwait(false);
+        }
+    }
+}
---
+++ Services/Exporter.cs
@@ -0,0 +1,21 @@
+﻿namespace CGTCalculator;
+
+internal static class Exporter
+{
+    internal static async Task ExportAsync(string fileName, List<Transaction> transactions)
+    {
+        using var fs = File.OpenWrite(fileName);
+        using var sr = new StreamWriter(fs);
+
+        await WriteLineAsync(sr, nameof(Field.Date), nameof(Field.Type), nameof(Field.Symbol), nameof(Field.Quantity), nameof(Field.Value));
+        foreach (var transaction in transactions)
+        {
+            await WriteLineAsync(sr, transaction.Date, transaction.Type, transaction.Symbol, transaction.Quantity, transaction.Value);
+        }
+    }
+
+    private static Task WriteLineAsync(StreamWriter sr, object v1, object v2, object v3, object v4, object v5)
+    {
+        return sr.WriteLineAsync(string.Join(",", v1, v2, v3, v4, v5));
+    }
+}
