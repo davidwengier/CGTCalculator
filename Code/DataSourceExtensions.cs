bff06f27a128f48218498d53f0f79dc87d17d222
David Wengier <david.wengier@microsoft.com>
2023-09-03 21:32:16 +1000

    Add a dropdown for symbols, and default select one
    Fixes #1

---
+++ Code/DataSourceExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace CGTCalculator;
+
+internal static class DataSourceExtensions
+{
+    public static List<string> GetAllSymbols(this DataSource dataSource)
+    {
+        return dataSource.Transactions.Select(x => x.Symbol).Distinct().ToList();
+    }
+}
